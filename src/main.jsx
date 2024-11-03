a112ef771558933c2ebeec241eddc4f45965fba4
Tarikuzzaman12 <tarikuzzamantuhin920@gmail.com>
2024-11-03 19:46:30 +0600

    Basic  React setup

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from './Layout/MainLayout';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+       <RouterProvider router={router} />
+  </StrictMode>,
+)
